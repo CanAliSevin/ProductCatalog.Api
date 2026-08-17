6b99ec4b5f4d30c9a97c38889a17338265a50ed5
Can Ali  Sevin <sevinncan@gmail.com>
2026-08-17 19:00:33 +0300

    kod incelendi

--- obj/Debug/net10.0/ProductCatalog.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProductCatalog.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductCatalog.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce2f7dc82b44b26a42cffb3f49ff04a39572c835")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54f57a578887ff19f706ef45516a07d8b19c29d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductCatalog.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductCatalog.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
