4c134bfc336c2c952746b1626d6254ebe5c03f32
Can Ali  Sevin <sevinncan@gmail.com>
2026-08-21 18:36:25 +0300

    Update database configuration and migrations

--- obj/Debug/net10.0/ProductCatalog.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProductCatalog.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductCatalog.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b339ee66de560829404e9dba29772f65a5a70991")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08de51e2da587e1f780153f0eaf05532610e4924")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductCatalog.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductCatalog.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
