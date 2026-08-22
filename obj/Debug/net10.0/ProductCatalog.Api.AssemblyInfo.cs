e976576639bf1a17902116968a52f32a70342f63
Can Ali  Sevin <sevinncan@gmail.com>
2026-08-22 23:03:02 +0300

    sorunlar giderilid

--- obj/Debug/net10.0/ProductCatalog.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProductCatalog.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductCatalog.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea1de856b6aa488f5b6a699f21ae91f41b8de56e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3c962582b339d3dba66b6fcae87bfc778f84d7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductCatalog.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductCatalog.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
